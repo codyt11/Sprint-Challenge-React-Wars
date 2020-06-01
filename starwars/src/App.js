578177e4e1975e9a4984ca1f6ad0e412cb38be7d
Codyt11 <Codyt11@hotmail.com>
2020-05-31 21:48:29 -0500

    MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,31 @@
-import React from 'react';
+
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+import styled from 'styled-components';
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+function App() {
+  const [characters, setCharacters] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  useEffect(() => {
+    axios.get("https://rickandmortyapi.com/api/character")
+    .then(response => {
+      console.log(response);
+      setCharacters(response.data.results);
+      console.log(response.data.results);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, [])
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+
+return (
+  <div className = "app">
+    <Character  characters = {characters}/>
+  </div>
+
   );
 }
-
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,42 @@
 // Write your Character component here
+import React from "react";
+import styled from 'styled-components';
+
+
+const Character = (props) => {
+    const { characters } = props;
+
+        const StyledDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    padding: 2%;
+    border: 1px solid black;
+    border-radius: 6px;
+    background-color: yellow;
+    font-size: 1.8rem;
+    width: 50%;
+    margin: 2.5% auto;
+
+    `;
+ 
+    
+    return (
+        <div className = "app">
+          {characters.map(character => {
+              return(
+                <StyledDiv key={character.id}>
+                  <h2>{character.name}</h2>
+                  <p>{character.status}</p>
+                  <img src={character.image} alt={`${character.name}'s face`}/>
+                </StyledDiv>
+              )
+            }
+          )}
+          </div>
+    )
+    }
+
+
+export default Character;
